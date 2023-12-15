e85e9bf554c7100c6d1c1cadf5085dad54c511a5
Gustav Schölin <gustav@scholin.dev>
2023-12-15 23:37:15 +0100

    Add solutions for day 9 2023

---
+++ 2023/day_9/task_1.py
@@ -0,0 +1,14 @@
+from utils.read_input import read_input
+
+
+def interpolation(x):
+    if all([i == 0 for i in x]):
+        return 0
+    return x[-1] + interpolation([x[i + 1] - x[i] for i in range(len(x) - 1)])
+
+
+if __name__ == "__main__":
+    readings = [
+        [int(i) for i in line.split()] for line in read_input("2023/day_9/input.txt")
+    ]
+    print(sum([interpolation(r) for r in readings]))
